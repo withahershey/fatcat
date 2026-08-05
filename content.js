0fd0334476fb8a4d6d37c422a2ef384461706c63
withahershey <harshith.sam@outlook.com>
2026-08-05 19:07:25 -0400

    dx

---
+++ content.js
@@ -0,0 +1,8 @@
+const div = document.createElement('div');
+div.style.backgroundColor = 'yellow';
+div.textContent = 'fat cat';
+div.style.position = 'fixed';
+div.style.top = '0';
+div.style.right = '0';
+div.style.zIndex = '9999';
+document.body.appendChild(div);
