85b2cb463c7780f3a14fa8480108e21752ece366
withahershey <harshith.sam@outlook.com>
2026-08-15 13:47:10 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,17 +1,20 @@
 var storageTabs;
-
+var next = false;
 async function init() {
-    // var numTabs = 0;
-    storageTabs = await chrome.storage.local.get({ numTabs: 0 });
+  var number = await chrome.tabs.query({}).length;
+  storageTabs = await chrome.storage.local.get({ numTabs: number });
+  next = true;
 }
 init();
-chrome.tabs.onCreated.addListener(
-  count
-);
-chrome.tabs.onRemoved.addListener(
-  uncount
-);
 
+if (next) {
+  chrome.tabs.onCreated.addListener(
+    count
+  );
+  chrome.tabs.onRemoved.addListener(
+    uncount
+  );
+}
 async function count() {
   storageTabs.numTabs++;
   await chrome.storage.local.set({ numTabs: storageTabs.numTabs });
