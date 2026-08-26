798b44dbe50fbaf4238ea129f2be5b90cde29bf4
withahershey <harshith.sam@outlook.com>
2026-08-26 18:21:25 -0400

    d

--- content.js
+++ content.js
@@ -1,11 +1,49 @@
 var img = document.createElement('div');
-img.style.backgroundImage = "url('" + chrome.runtime.getURL("img/cat1.png") + "')";
+// img.style.backgroundImage = "url('" + chrome.runtime.getURL("img/cat1.png") + "')";
 // img.style.backgroundColor = 'rgb(255, 0, 0)';
-img.style.backgroundSize = 'cover';
-img.style.width = '550px';
-img.style.height = '300px';
+img.style.backgroundRepeat = 'no-repeat';
+img.style.backgroundSize = 'contain';
+img.style.width = '200px';
+img.style.height = '100px';
 img.style.position = 'fixed';
 img.style.top = 0;
 img.style.right = '50px';
-img.style.zIndex = 10000;
+img.style.zIndex = 2147483647;
+img.style.pointerEvents = 'none';
+
+
+async function updateImage() {
+    var storageTabs = await chrome.storage.local.get('numTabs');
+    var tabcount = storageTabs.numTabs;
+    var stage = 1;
+    if (tabcount >= 0 && tabcount <= 5) {
+        stage = 1;
+    }else if (tabcount >= 6 && tabcount <= 10) {
+        stage =2;
+    }else if (tabcount >= 11 && tabcount<=15){
+        stage = 3;
+    }else if (tabcount>=16 && tabcount<=18){
+        stage = 4;
+    }else if (tabcount>=19 && tabcount<=20){
+        stage = 5;
+    }else if (tabcount>=21 && tabcount<=25){
+        stage = 6;
+    }else if (tabcount>=26 && tabcount<=30){
+        stage = 7;
+    }else if (tabcount>=31 && tabcount<=35){
+        stage = 8;
+    }else if (tabcount>=36 && tabcount<=45){
+        stage = 9;
+    }else if (tabcount>=46 && tabcount<=50){
+        stage = 11;
+    }
+    console.log("tabcount:", tabcount, "stage:", stage);
+    img.style.backgroundImage = "url('" + chrome.runtime.getURL("img/cat" + stage + ".png") + "')";
+}
+
+updateImage();
 document.body.appendChild(img);
+
+chrome.storage.onChanged.addListener(function() {
+  updateImage();
+});
